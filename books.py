69d89d74ac6bca8aa45563987b62888b78bbb3b7
Diana Coelho <coelhodiana04@gmail.com>
2024-05-30 22:50:11 -0300

    feat: post request

--- books.py
+++ books.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
 
 app = FastAPI()
 
@@ -40,3 +40,7 @@ async def read_author_by_category(book_author: str, category: str):
                         books_to_return.append(book)
 
                 return books_to_return
+        
+@app.post("/books/create_book")
+async def create_book(new_book=Body()):
+        BOOKS.append(new_book)
